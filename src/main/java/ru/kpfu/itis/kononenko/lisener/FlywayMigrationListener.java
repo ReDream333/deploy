bf355ea4922e77f5f0b57fef939431c1764fa683
konon <kononenkoyyulia@gmail.com>
2024-12-21 06:33:53 +0300

    try 2

--- src/main/java/ru/kpfu/itis/kononenko/lisener/FlywayMigrationListener.java
+++ src/main/java/ru/kpfu/itis/kononenko/lisener/FlywayMigrationListener.java
@@ -1,32 +1,33 @@
-//package ru.kpfu.itis.kononenko.lisener;
-//
-//import jakarta.servlet.ServletContextEvent;
-//import jakarta.servlet.ServletContextListener;
-//import jakarta.servlet.annotation.WebListener;
-//import lombok.extern.slf4j.Slf4j;
-//import org.flywaydb.core.Flyway;
-//
-//
-//@Slf4j
-//@WebListener
-//public class FlywayMigrationListener implements ServletContextListener {
-//
-//    private static final String URL_KEY = "postgresql://postgres:zmlOdCfhHMUeRkDzzmeOEQNCjRDjMxxC@postgres.railway.internal:5432/railway";
-//    private static final String USERNAME_KEY = "postgres";
-//    private static final String PASSWORD_KEY = "zmlOdCfhHMUeRkDzzmeOEQNCjRDjMxxC";
-//
-//    public void contextInitialized(ServletContextEvent sce) {
-//        try{
-//            Flyway flyWay = Flyway
-//                    .configure()
-//                    .dataSource(URL_KEY, USERNAME_KEY, PASSWORD_KEY)
-//                    .load();
-//            flyWay.migrate();
-//            log.info("Flyway migration complete.");
-//        } catch (Exception e){
-//            log.error(e.getMessage());
-//        }
-//    }
-//
-//
-//}
+package ru.kpfu.itis.kononenko.lisener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+
+
+@Slf4j
+@WebListener
+public class FlywayMigrationListener implements ServletContextListener {
+
+    private static final String URL_KEY = "jdbc://postgres:bmVnPtEmWyUFbAVDZXIPagHcCxYjqjJb@postgres.railway.internal:5432/railway";
+    private static final String USERNAME_KEY = "postgres";
+    private static final String PASSWORD_KEY = "zmlOdCfhHMUeRkDzzmeOEQNCjRDjMxxC";
+
+    public void contextInitialized(ServletContextEvent sce) {
+        try{
+            Flyway flyWay = Flyway
+                    .configure()
+                    .dataSource(URL_KEY, USERNAME_KEY, PASSWORD_KEY)
+                    .load();
+            flyWay.migrate();
+            log.info("YES MIGRATION");
+        } catch (Exception e){
+            log.error(e.getMessage());
+            log.error("WHAT NOOOOOOOOOO");
+        }
+    }
+
+
+}
